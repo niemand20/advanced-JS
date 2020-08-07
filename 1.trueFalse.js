d2f5470bf77f1e232841d4a4a0bacb76ced6b7fa
niemand20 <zahid17290@gmail.com>
2020-08-07 04:26:54 +0100

    true and false

---
+++ 1.trueFalse.js
@@ -0,0 +1,27 @@
+let name=[];
+//console.log(name2);
+
+//const name = 04;
+if (name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+/*note
+Truthies:
+else if  name = 54(any natural number other than 0), this is truthy.
+
+else if name = "0" non-empty string, this is truthy.
+name = [],{}
+name = "false">true
+
+Falsies:
+else if name = "" empty string, this is falsy.
+here, if  name = 0, it is falsy.
+name=false
+undefined is false
+null is false.
+NaN is false
+
+*/
