ab05023cf4283d23e01252eba3fc8cac81c1a417
niemand20 <zahid17290@gmail.com>
2020-08-07 05:15:44 +0100

    null-vs-undefined cleared

---
+++ 3.nullVSundefined.js
@@ -0,0 +1,31 @@
+ let pakhi;
+ console.log(pakhi);
+
+ function add(x,y){
+     console.log(x+y);
+ }
+ result = add(12,34);
+ console.log(result);
+
+
+undefined:
+1.if a function is not returned, it will be undefined
+2.if a function is returned without a value it is undefined
+3.if two parameters are given and one parameter is passed, it is undefined
+
+
+function add(num1,num2){
+     console.log(num1,num2)
+ }
+const result = add(10)
+ console.log(result);
+
+
+ const premik = {name: "sagir", phone:234}; 
+ console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+null:
+null should be declared explicitly .
