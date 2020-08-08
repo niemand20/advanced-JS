34d1ceb1c1986ec55f79ca6c8e63813aa15d47b2
niemand20 <zahid17290@gmail.com>
2020-08-08 06:17:45 +0100

    scope

---
+++ 7.scope.js
@@ -0,0 +1,17 @@
+
+
+function sum(first, second){
+    let result = first+ second + bonus;
+    //console.log(bonus);
+    if (result>9){
+        var   mood = "happy";
+         var mood = "sad";
+        var mood = "funky";
+         console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+const bonus = 20
+const total = sum(20,30)
+console.log(total);
