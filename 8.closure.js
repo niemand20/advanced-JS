3a05ac5301764b6806cbd45c69a8fd2568dee0ea
niemand20 <zahid17290@gmail.com>
2020-08-08 08:03:42 +0100

    closure js

---
+++ 8.closure.js
@@ -0,0 +1,21 @@
+function stopWatch(){
+    let count = 0;
+    return function s(){
+        
+        count++;
+        return count;
+    }
+}
+
+const clock1 = stopWatch();
+console.log(clock1());
+//clock1();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = stopWatch();
+console.log(clock2());
+console.log(clock2());
+console.log(clock1());
